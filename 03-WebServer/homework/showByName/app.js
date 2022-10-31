466100deda03aaf75c7ed3c793298b0e963ffb41
Adolfo Fabian Calle <86446298+fabicalle@users.noreply.github.com>
2022-10-31 19:54:56 -0300

    M3

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,18 @@
-var fs  = require("fs")
-var http  = require("http")
+var fs = require("fs")
+var http = require("http")
 
 // Escribí acá tu servidor
+
+http.createServer((req, res) => {
+    const name = req.url.slice(1); 
+    const files= fs.readdirSync("./images");
+    for(const file of files){
+    if(file.includes(name)){
+        res.writeHead(200,{"Content-type":"image/jpg"})
+const img= fs.readFileSync(`./images/${name}_doge.jpg`)
+       return res.end(img)
+    }
+}
+return res.writeHead(404,).end("Image not found")
+
+}).listen(3002, "localhost");
